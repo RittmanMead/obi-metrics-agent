5d2c7d17a5a8e356acddb8250331d0e211b4b1c0
Robin Moffatt <robin.moffatt@rittmanmead.com>
2014-03-18 13:18:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,205 @@
+# obi-metrics-agent.py							@rmoff / March 2014
+
+## LICENCE TO GO HERE
+
+
+## Introduction
+This is the same concept as Venkat’s original code. It differs as follows: 
+
+* Single collector script (`obi-metrics-agent.py`), re-written in python
+* In-built scheduler/timer
+* Multiple output options: 
+	* CSV
+	* INSERT statements
+	* Carbon, (for rendering in Graphite etc)
+	* Unparsed XML
+	* Unparsed raw opmn output
+* Parse data as it is collected, or write to disk
+* Parse data collected previously
+
+As well as being core to performance testing, it could be used as a standard OBI monitoring tool (alongside EM etc).
+
+---
+
+## About the collector
+### Which OBI metrics are collected?
+All of the ones that OPMN supports. Currently, BI Server and BI Presentation Services, plus the opmn process metrics (CPU time of each OBI component)
+### What about other metrics?
+
+* OS metrics : collectl -> Carbon/Graphite  (or JMeter PerfMon)
+* JVM metrics: Mission Control? 
+### How are the metrics collected? 
+Using the documented OPMN functionality to expose DMS metrics (`opmnctl metric op=query`)
+
+### What are the installation requirements?
+
+* Python (2.4.3, the version that ships on OEL 5.5, is fine)
+* lxml
+	* Could be optional, if only gathering raw data for processing later. Requires code change.
+
+### Which platforms are supported?
+
+* Tested thoroughly on OEL 5.5 and OL6.3
+* Seems to work on Windows 2003
+
+Python is cross-platform, so it ought to work on other platforms
+
+### Known Issues (11.1.1.6)
+
+There is a bug in the opmn process which causes corrupt XML sometimes. On the Exalytics box, this can sometimes be as much as 15% of samples. An SR was opened for this, SR 3-5811527931.
+
+On corrupt samples, the datapoint is just dropped. 
+
+The patch for this issue is 13055259. If patching the sample environment is not an option, then dropped data points will have to be accepted. 
+
+### Limitations
+Only single-node clusters are currently supported. Modification of the parsing code would probably be necessary to handle additional instances. 
+
+### Installation on Unix
+
+1. Put `obi-metrics-agent.py` into its own folder, for example, `/u01/app/obi-metrics-agent/`
+	
+	`git clone git@github.com:RittmanMead/scripts.git`
+	
+2. Check that python is available and at least v2.4.3:  
+
+	python -V
+	
+3. Install lxml 
+
+	sudo yum install python-devel python-setuptools libxml2-devel libxslt-devel
+	sudo easy_install lxml
+	
+### Installation on Windows
+1. Put `obi-metrics-agent.py` into its own folder, for example, `C:\ltpma\`
+2. If not already, install Python: 
+		* Windows x86: http://www.python.org/ftp/python/2.7.3/python-2.7.3.msi
+		* Windows x86-64: http://www.python.org/ftp/python/2.7.3/python-2.7.3.amd64.msi
+3. Install lxml (http://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml). Make sure it's the right version (tested on 2.3) for the right python (2.7) and architecture (32/64)
+		* Windows x86: lxml-2.3.4.win32-py2.7.exe
+		* Windows x86-64: http://www.lfd.uci.edu/~gohlke/pythonlibs/eodybnto/lxml-2.3.6.win-amd64-py2.7.exe
+
+### Optional : install collectl (not Windows)
+
+`sudo yum install collectl`
+
+---
+
+## Using the collector
+### Introduction
+
+**obi-metrics-agent** collects OBI metrics data from DMS via opmn. It has three modes:  
+
+1. Collect and parse metrics to output
+2. Collect metrics to disk only
+3. Parse existing metrics from disk
+
+### Which mode to use?
+
+1. Collect and parse metrics to output
+	* Near-real-time rendering of OBI metrics in Graphite
+	* Near-real-time load of data into Oracle (via CSV & external table)
+2. Collect metrics to disk only
+	* For analysis at a later date
+	* If graphite/carbon is not available to send data to
+	* If outbound network bandwidth is constrained (or could be, by OBIEE)
+	* For lowest host system overhead
+3. Parse existing metrics from disk
+	* Parse previously collected data and output to Graphite or Oracle (via CSV & external table)
+
+### Example usage
+#### Collect and Parse - output to Carbon/Graphite
+
+1. Initial Set up: 
+	* Make sure there is a Carbon/Graphite server available (two easy options : Existing Amazon EC2 AMI, or Ubuntu VirtualBox image). Use `test_carbon.py` to test it if required
+	* Configure the Carbon server’s /opt/graphite/conf/storage-schemas.conf for sample intervals you want to use eg 5 sec
+2. Run **collectl** to send OS data to the Carbon server:   
+
+		collectl -scdmnCDZ --export graphite,graphite_server:2003,p=.os.,s=cdmnCDZ
+3. Run **obi-metrics-agent.py**
+
+		obi-metrics-agent.py --output carbon --carbon-server graphite_server
+4. View Graphite dashboards, by default at `http://<graphite_server>/dashboard/`
+
+#### Collect and Parse - output to Oracle/OBIEE
+
+1. Initial Set up:
+	1. Edit `ddl.sql` CREATE DIRECTORY definition to point to the folder that holds the csv file (`data-directory` specified to obi-metrics-agent.py, see below)
+	2. Run `ddl.sql` as SYSDBA. It will create a LTPMA user.
+2. Collect OS stats with **collectl** to disk, or **JMeter PerfMon**
+3. Run **obi-metrics-agent.py**
+
+		obi-metrics-agent.py --output csv --data-directory ~/data
+4. Viewing the output:
+	1. Data can be queried directly through Oracle as OBI_METRICS_EXT
+	2. Use Create Table As Select (CTAS) to copy data to permanent Oracle table
+	3. Use OBIEE or alternative tool to render the data from the table
+		
+#### Collect metrics to disk (for processing at a later date)
+1. Initial Set up: none
+2. Collect OS stats with **collectl** to disk, or **JMeter PerfMon**
+3. Run `obi-metrics-agent.py`
+
+		obi-metrics-agent.py --output raw --data-directory ~/data
+
+#### Parse existing metrics from disk, output to CSV file
+1. Run `obi-metrics-agent.py`
+
+		obi-metrics-agent.py --output csv --parse-only
+
+#### Parse existing metrics from disk, output to Carbon/Graphite
+1. Run `obi-metrics-agent.py`
+	
+		obi-metrics-agent.py --parse-only --output carbon --carbon-server localhost 
+NB this may flood carbon and might need throttling; not clear how carbon handles an influx of data. *Maybe update obi-metrics-agent to send pickles for efficiency?*
+
+#### Example usage on Windows
+
+*Calling obi-metrics-agent on Windows is no different from calling it on Linux. Depending on how Python has been installed you may need to directly reference the Python executable*
+
+	c:\Python27\python.exe c:\Users\Administrator\Documents\GitHub\ltpma\obi-metrics-agent\obi-metrics-agent.py --opmnbin c:\oracle\middleware\instances\instance1\bin\opmnctl.bat --output carbon --carbon-server rnm-ubu-01 --fmw-instance=WIN2k8
+
+
+
+---
+		
+## Architecture
+￼
+![Architecture](architecture.png)
+
+## Rendering the data
+### Graphite
+Graphite is an open-source graphing platform specifically for time-based metrics. It includes its own database (“whisper”) and database collector agent (“carbon”). 
+
+Graphite is an optional target for obi-metrics-agent. The obi-metrics-agent can be deployed standalone.
+
+Graphite as it is also supported by collectl, an OS monitoring tool favoured by the likes of Kevin Closson and Greg Rahn. 
+
+Using graphite, any metrics held in its database can quickly be rendered into a graph. Because Graphite is all about time-based measurement, we can easily overlay OBI metrics with OS metrics in the same graphs. 
+
+￼![Graphite screenshot](graphite01.png)
+
+### Why not use OBIEE for drawing graphs? 
+We can, if we want. The OBI metric data can be loaded in by external table from the CSV files, or using the generated INSERT statements. 
+
+Using Graphite gives an alternative to a dependency on OBIEE because rendering the data on the system that we’re also testing complicates things. If we then set up a second OBIEE server just for rendering graphs then it opens up the question of what’s the best graphing tool for this particular job, and Graphite is a strong option here. 
+
+### What are the system requirements for Graphite?
+
+* Python 2.6
+* Plus several other components.
+
+I am currently running Graphite on an Ubuntu VM, and also have an Amazon Linux AMI with it available. It will work on OL6 but not natively on OL5 because of the early python version.
+
+### Installing Graphite
+* [Cribsheet 1](https://www.evernote.com/shard/s16/sh/7eb00bd7-2832-40ab-825f-1728cc568ca1/ca24ccc31636181150f210c1ff2c7e44)
+* [Cribsheet 2](https://www.evernote.com/shard/s16/sh/4beff8ea-efd0-401d-b219-552db74246c9/118eec530d763a3a88afb68b58b13446)
+* [Cribsheet 3](https://www.evernote.com/shard/s16/sh/32d67a7d-bb9d-4257-adf9-746ee0ec9174/b0ed92f849279a1ea7785eede4811c8e)
+
+### Using Graphite
+* [[ build metric graphs in composer ]]
+* [[ set relative or absolute time ]]
+* [[ Set time autorefresh ]]
+* [[ build up dashboards from graphs ]]
+* [[ export sqlite DB of predefined OBI/OS dashboards of standard metrics ]]
+
---
+++ obi-metrics-agent.py
@@ -0,0 +1,401 @@
+#!/usr/bin/python
+#
+# obi-metrics-agent.py
+#
+# @rmoff / March 2014
+# Inspired by Venkat's original java implementation
+#
+# [[ LICENCE HERE ]]
+#
+# -------------------------------------------------
+# Purpose: 
+# This script gathers OBI metrics and sends them to Carbon for graphing in Graphite, or output to file (unprocessed XML including header, or CSV)
+#
+# CSV file could be used as source for external table to get the data into Oracle easily
+#
+# ---
+# Known problems: 
+#   If OBI Scheduler isn't running, parsing OPMN process metrics will fail (**Error parsing metric data: no such child: noun)
+#   invalid_files.txt doesn't get written to 
+#
+# XML file structures currently handled:
+#  OPMN OBIS, OBIPS metrics
+#  OPMN process metrics
+#
+# Usage:
+#  obi-metrics-agent.py
+#
+# Pre-requisites : lxml
+#   sudo yum install libxml2-devel 
+#   sudo easy_install lxml
+#
+# To Do:
+#  Refactor into proper functions/procs
+#  Error handling
+#  Proper configuration file and checking
+#
+#
+import time
+import os
+import sys
+import subprocess
+import socket
+from glob import glob
+from optparse import OptionParser
+from lxml import objectify
+
+def parse_xml(xml):
+	metrics_msg = []
+	metrics_list = []
+	global valid_xml
+	global invalid_xml
+	try:
+		root = objectify.fromstring(xml)
+
+		# Parse XML
+		host = root.attrib['host']
+		if FMW_INSTANCE is not None:
+			host = '%s.%s' % (host,FMW_INSTANCE.upper())
+		header_process_name = root.attrib['name']
+		ts_epoch_msec = root.attrib['timestamp']
+		ts_epoch = int(ts_epoch_msec)/ 1000
+		ts= time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(ts_epoch))
+
+		if (header_process_name == 'Oracle BI Server' or header_process_name == 'Oracle BI Presentation Server') :
+			# Assumption is that this is a BIS or BIPS pdml file
+			# BI Metrics
+			n= root.statistics.noun[0]
+			for n1 in root.statistics.noun[0].noun:
+				group = (n1.attrib['type'] + "." + n1.attrib['name'])
+				for metric in n1.metric:
+					metric_name = metric.attrib['name'].replace('.value','').replace('.','').replace('/','-')
+					metric_value= metric.value
+					metric_full_name = (host + ".OBI." + group + "." + metric_name).replace(' ','_')
+					metric_out = metric_full_name + " " + str(metric_value) + " " + str(ts_epoch)
+					metrics_list.append((metric_full_name, str(metric_value) , str(ts_epoch)))
+					metrics_msg.append(metric_out)
+
+			# DMS Process metrics
+			n= root.statistics.noun[1]
+			group = (n.attrib['type'] + "." + n.attrib['name'] + "." + header_process_name)
+			for metric in root.statistics.noun[1].metric:
+				metric_type = metric.value.attrib['type']
+				if metric_type == 'string':
+					pass
+				else:
+					metric_name = metric.attrib['name'].replace('.value','')
+					metric_value= metric.value
+					metric_full_name = (host + ".DMS." + group + "." + metric_name).replace(' ','_')
+					metric_out = metric_full_name + " " + str(metric_value) + " " + str(ts_epoch)
+					metrics_list.append((metric_full_name, str(metric_value) , str(ts_epoch)))
+					metrics_msg.append(metric_out)
+		elif header_process_name == 'opmn' :
+			# OPMN Metrics
+			instance = root.statistics.noun[0].noun[1].noun[1]
+			for component in instance.noun:
+				process_type = component.noun[0]
+				process_set = process_type.noun[0]
+				process_name = process_set.attrib['name']
+				for metric in process_set.metric:
+					metric_type = metric.value.attrib['type']
+					if metric_type == 'string':
+						pass
+					else:
+						metric_full_name = host + '.Process.' + metric.attrib['name'].replace('.value','').replace('.count','') + '.'+ process_name
+						metric_value = metric.value
+						metric_out = metric_full_name + " " + str(metric_value) + " " + str(ts_epoch)
+						metrics_list.append((metric_full_name, str(metric_value) , str(ts_epoch)))
+						metrics_msg.append(metric_out)
+				for metric in process_set.noun[0].metric:
+					metric_type = metric.value.attrib['type']
+					if metric_type == 'string':
+						pass
+					else:
+						metric_full_name = host + '.Process.' + metric.attrib['name'].replace('.value','').replace('.count','') + '.'+ process_name
+						metric_value = metric.value
+						metric_out = metric_full_name + " " + str(metric_value) + " " + str(ts_epoch)
+						metrics_list.append((metric_full_name, str(metric_value) , str(ts_epoch)))
+						metrics_msg.append(metric_out)
+		else :
+			print ' *** PDML not recognised *** '
+			print ' Name in header : ' % (header_process_name)
+			return 3
+
+
+	except Exception, err:
+		invalid_xml += 1
+		sys.stderr.write('\t**Error parsing metric data: %s\n' % str(err))
+		try:
+			file = open('%s/invalid_files.txt' % (LTPMA_DATA),'a')
+			file.write(filename + '\n')
+			file.close()
+		except:
+			pass
+		return 2
+
+	valid_xml += 1
+	# more output conditions here - reformat into CSV (easy) and INSERT statements (not so easy)
+	print '\t\t Processed : \t%d data values @ %s \t%s' % (len(metrics_msg),ts,header_process_name)
+	if (len(metrics_msg) > 0) :
+		# Do something with the data
+		if do_output_csv:
+			csvline = '\n'.join(metrics_msg).replace(' ',',') + '\n'
+			csvfile = open('%s/metrics.csv' % (LTPMA_DATA),'a')
+			csvfile.write(csvline)
+			print '\t\t\tAppended CSV data to %s' % (csvfile.name)
+			csvfile.close()
+		if do_output_carbon:
+			try:
+				# Send data to carbon
+				carbon_message = '\n'.join(metrics_msg) + '\n'
+				sock = socket.socket()
+				sock.settimeout(5)
+				sock.connect((CARBON_SERVER, CARBON_PORT))
+				#print 'sending message:\n%s' % message
+				#print 'Sending message to carbon server %s' % (CARBON_SERVER)
+				sock.sendall(carbon_message)
+				sock.close()
+				print '\t\t\tSent data to Carbon at %s' % (CARBON_SERVER)
+			except Exception, err:
+				sys.stderr.write('\t**Error sending data to Carbon: %s\n' % str(err))
+		if do_output_sql:
+			try:
+				# Write an INSERT statement
+				sql =''
+				for line in metrics_list:
+					metr,value,ts = line
+					sql += "INSERT INTO OBI_METRICS (METRIC,VALUE,TIME_EPOCH) VALUES('%s',%d,%d);\n" % (metr,int(value),int(ts))
+				sqlfile = open('%s/inserts.sql' % (LTPMA_DATA),'a')
+				sqlfile.write(sql)
+				print '\t\t\tWritten SQL INSERT statement to %s' % (sqlfile.name)
+				sqlfile.close()
+			except Exception, err:
+				sys.stderr.write('\t**Error writing SQL: %s\n' % str(err))
+	else:
+		print '(No data - no action)'
+	return 0
+
+def get_metrics(opmn_cmd,outfile):
+	try:
+		component='COMPONENT_NAME=%s' % (opmn_cmd)
+		outputfile=open(outfile,'w')
+		p = subprocess.Popen([OPMN_BIN,'metric','op=query',component,'format=xml'],stdout=outputfile)
+		rc = p.wait()
+		#print '%s\t%d' % (opmn_cmd,outputfile.tell())
+		outputfile.close()
+		outputfile=open(outfile,'r')
+		out = outputfile.read()
+		outputfile.close()
+		return out
+	except Exception, err:
+		sys.stderr.write('\n\t**get_metric : Error calling OPMN: %s\n' % str(err))
+		sys.exit()
+
+def collect_metrics():
+	try:
+		p = subprocess.Popen([OPMN_BIN,'status'],stdout=subprocess.PIPE)
+		p.wait()
+		opmnout = p.stdout.read()
+		p.stdout.close()
+		if opmnout.find('opmn is not running')>-1:
+			sys.stderr.write('\nExiting : \n\t**OPMN is not running, so metrics cannot be collected\n')
+			sys.exit()
+	except Exception, err:
+		sys.stderr.write('\n\tcollect_metrics: Error calling OPMN \n%s\n' % str(err))
+		sys.exit()
+
+	while True:
+		thisTime = time.time()
+		nextTime = thisTime + interval
+		print '\n--Gather metrics--'
+		print "\tTime of sample: %s (%d)\n " % (time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(thisTime)),thisTime)
+		for cmd in opmn_cmds:
+			component=cmd
+
+			# Call the opmn command
+			if do_output_raw:
+				filename='%s/%s_%d.raw' % (LTPMA_DATA,component,thisTime)
+			else:
+# Need to make this cross-platform
+#				filename='/dev/shm/obimetric.tmp'
+				filename='%s/data.raw' % (LTPMA_DATA)
+			print '\tGet metrics for %s' % (component)
+			raw = get_metrics(cmd,filename)
+			if len(raw) < 100:
+				print '\n\t ** Output returned from opmn is unexpectedly short, is there a problem? Output follows: \n\t\t%s' % (raw)
+			if do_output_raw:
+				print '\t\t\tWritten raw output to %s' % (filename)
+
+			# Strip the first 101 bytes from the response - this is the HTTP header
+			xml=raw[101:]
+			if do_output_xml:
+				# dump XML to file
+				filename='%s/%s_%d.xml' % (LTPMA_DATA,component,thisTime)
+				xml_file=open(filename,'w')
+				xml_file.write(xml)
+				xml_file.close()
+				print '\t\t\tWritten XML output to %s' % (filename)
+
+			# Process the XML returned from OPMN
+			if do_output_sql or do_output_carbon or do_output_csv:
+				parse_xml(xml)
+				total_xml=valid_xml+ invalid_xml
+				if total_xml == 0:
+					perc_invalid_xml=0
+				else:
+					perc_invalid_xml = (invalid_xml / total_xml) *100
+					perc_valid_xml = (valid_xml / total_xml) *100
+
+		if do_output_sql or do_output_carbon or do_output_csv:
+			print '\n\tProcessed: %d\tValid: %d (%0.2f%%)\tInvalid: %d (%0.2f%%)' % (total_xml,valid_xml,perc_valid_xml,invalid_xml,perc_invalid_xml)
+		print '\t-- Sleeping for %d seconds (until %d)--' % (interval,time.time())
+		while nextTime > time.time():
+			time.sleep((nextTime-time.time()))
+
+def parse_files():
+	# How to express raw OR xml in a single regex? would need to cater for raw files and stripping header, but rest of code could be combined
+	base_path = '%s%s' % (LTPMA_DATA,os.sep)
+
+	raw_files_path = base_path + '*.raw'
+	raw_files = glob(raw_files_path)
+	if len(raw_files) > 0 :
+		print 'Processing raw files:'
+		for filename in raw_files:
+			print filename
+			file = open(filename,'r')
+			xml = file.read()[101:]
+			file.close()
+			if parse_xml(xml) == 0:
+				os.rename(filename,filename +'.processed')
+				print '\tSuccessfully processed %s and renamed it to %s' % (filename,filename +'.processed')
+			else:
+				os.rename(filename,filename +'.err')
+				print '\tError processing %s. File renamed to %s' % (filename,filename +'.err')
+	else:
+		print '\n** No raw files found to process in %s\n' % (raw_files_path)
+
+	xml_files_path = base_path + '*.xml'
+	xml_files = glob(xml_files_path)
+	if len(xml_files) > 0 :
+		print 'Processing xml files:'
+		for filename in xml_files:
+			print filename
+			file = open(filename,'r')
+			xml = file.read()
+			file.close()
+			if parse_xml(xml) == 0:
+				os.rename(filename,filename +'.processed')
+				print '\tSuccessfully processed %s and renamed it to %s' % (filename,filename +'.processed')
+			else:
+				os.rename(filename,filename +'.err')
+				print '\tError processing %s. File renamed to %s' % (filename,filename +'.err')
+	else:
+		print '\n** No xml files found to process in %s\n' % (xml_files_path)
+
+
+###########################################
+
+###########################################
+
+###########################################
+
+###########################################
+
+
+
+# Internal Variable initialisations
+valid_xml=float(0)
+invalid_xml=float(0)
+
+opmn_cmds = ['coreapplication_obips1' ,'coreapplication_obis1','opmn']
+#opmn_cmds = ['coreapplication_obips2' ,'coreapplication_obis2','coreapplication_obips1' ,'coreapplication_obis1','opmn']
+
+# OptionParser is depreciated in Python 2.7, but used here for compatibility with Python 2.4 (the version distributed with OEL 5.5)
+# NB it does not support newlines (\n) but these are included for when I figure out how to implement them
+opts = OptionParser()
+opts.description = "obi-metrics-agent will can extract metric data from Fusion MiddleWare (FMW) and its Dynamic Monitoring Systems (DMS).\nIt will poll at a predefined interval and parse the resulting set of metrics.\nThe metrics can be output to a variety of formats, including CSV. Sending to Carbon is also supported, from where graphs can be rendered in Graphite."
+# epilog doesn't display, why?
+opts.epilog = ("obi-metrics-agent is (c) RittmanMead Limited and may not be used without permission")
+opts.add_option("-o","--output",action="store",dest="outputformat",default="csv",help="The output format(s) of data, comma separated. More than one can be specified.\nUnparsed options: raw, xml\nParsed options: csv , carbon, sql")
+opts.add_option("-d","--data-directory",action="store",dest="LTPMA_DATA",default="./data",help="The directory to which data files are written. Not needed if sole output is carbon.")
+opts.add_option("-p","--parse-only",action="store_true",dest="parse_only",default=False, help="If specified, then all raw and xml files specified in the data-directory will be processed, and output to the specified format(s)\nSelecting this option will disable collection of metrics.")
+opts.add_option("--fmw-instance",action="store",dest="FMW_INSTANCE",help="Optional. The name of a particular FMW instance. This will be prefixed to metric names.")
+opts.add_option("--carbon-server",action="store",dest="CARBON_SERVER",help="The host or IP address of the Carbon server. Required if output format 'carbon' specified.")
+opts.add_option("--carbon-port",action="store",dest="CARBON_PORT",default=2003,help="Alternative carbon port, if not 2003.")
+opts.add_option("-i","--interval",action="store",dest="interval",default=5,help="The interval in seconds between metric samples.")
+opts.add_option("--opmnbin",action="store",dest="OPMN_BIN",help="The complete path to opmnctl. Watch out for spaces.")
+
+input_opts , args = opts.parse_args()
+
+try:
+	interval = int(input_opts.interval)
+	output=input_opts.outputformat
+	parse_only= input_opts.parse_only
+	LTPMA_DATA = input_opts.LTPMA_DATA
+	FMW_INSTANCE = input_opts.FMW_INSTANCE
+	CARBON_SERVER = input_opts.CARBON_SERVER
+	CARBON_PORT = int(input_opts.CARBON_PORT)
+	OPMN_BIN = input_opts.OPMN_BIN
+except Exception, err:
+	sys.stderr.write('\t**Error reading input options: %s\n' % str(err))
+	sys.exit()
+
+if FMW_INSTANCE is not None:
+	FMW_INSTANCE = FMW_INSTANCE.upper()
+
+# For several of the variables, it would be good to fall back on the environment variable if it exists. Maybe put the env var check before the add_option and use the env var if it exists as the default= value
+
+# If OPMN_BIN isn't specified, then try and check the environment variables
+if not parse_only:
+	if OPMN_BIN is None: 
+		try:
+			OPMN_BIN = os.environ['OPMN_BIN']
+		except:
+			# Try a default path
+			OPMN_BIN = '/u01/app/oracle/product/fmw/instances/instance1/bin/opmnctl'
+			if not os.path.isfile(OPMN_BIN):
+				print 'OPMN_BIN must be defined as an environment variable.\nIt can alternatively be specified by the command line parameter --opmnbin.'
+				print '\nSet manually, or update setEnv.sh and run it (. ./setEnv.sh)'
+				print '*** Exiting ***'
+				sys.exit()
+	else:
+		if os.path.isdir(OPMN_BIN):
+			OPMN_BIN = OPMN_BIN + '/opmnctl'
+do_output_raw = (output.find('raw') > -1)
+do_output_csv = (output.find('csv') > -1)
+do_output_xml = (output.find('xml') > -1)
+do_output_carbon= (output.find('carbon') > -1)
+do_output_sql= (output.find('sql') > -1)
+
+if do_output_carbon and (CARBON_SERVER is None):
+	sys.stderr.write('\n\t** Carbon server must be specified if carbon output is selected. \n\tUse --carbon-server and optionally --carbon-port (defaults to 2003)\n\n')
+	sys.exit()
+
+# Print header and variable states
+print '\n\n\t\tobi-metrics-agent.py\n\n\n[[ LICENCE TO GO HERE ]] \n\r@rmoff / March 2014\n\n'
+print '\n\n---------------------------------------'
+print 'Output format             : %s' % output
+print 'raw/csv/xml/carbon/sql    : %s/%s/%s/%s/%s' % (do_output_raw,do_output_csv,do_output_xml,do_output_carbon,do_output_sql)
+print 'Data dir                  : %s' % LTPMA_DATA
+print 'FMW instance              : %s' % FMW_INSTANCE
+print 'OPMN BIN                  : %s' % OPMN_BIN
+if do_output_carbon:
+	print 'Carbon server             : %s' % CARBON_SERVER
+	print 'Carbon port               : %d' % CARBON_PORT
+if parse_only:
+	print '\n\nOption parse_only selected, so no metrics will be gathered. \nThe program will process all data files (xml and raw), and then exit'
+else:
+	print 'Sample interval (seconds) : %d' % interval
+print '---------------------------------------\n'
+
+if not os.path.exists(LTPMA_DATA):
+	print '\n\t%s does not exist ... creating' % (LTPMA_DATA)
+	os.makedirs(LTPMA_DATA)
+
+# Check process list for collectl?
+# print '\n\n   **Don''t forget to start collectl**'
+
+if parse_only:
+	parse_files()
+else:
+	collect_metrics()
