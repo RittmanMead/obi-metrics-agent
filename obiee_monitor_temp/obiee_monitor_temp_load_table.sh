b7152e0e920759732a16c43cf9d70a89664a48da
rmoff <rmoff@asgard.local>
2015-12-21 13:58:26 +0000

    Move temp capture to new repo

--- obiee_monitor_temp/obiee_monitor_temp_load_table.sh
+++
@@ -1,2 +0,0 @@
-source /home/oracle/.bash_profile
-mv --no-clobber /u01/data/obi-temp/obiee.biee.du.tsv /u01/data/obi-temp/obiee.biee.du.tsv.stage && sqlldr userid=biee_biplatform/Oracle123@pdborcl control=/opt/obi-metrics-agent/obiee_monitor_temp/obiee_monitor_temp.ctl && rm /u01/data/obi-temp/obiee.biee.du.tsv.stage
